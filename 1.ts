40295d4cb812289226be0c73b1cbc5a223ad265b
SALEHIN <msionlinekingdom@gmail.com>
2024-12-14 18:20:01 +0600

    answerred 1

--- 1.ts
+++ 1.ts
@@ -1,3 +1,13 @@
 {
     //solving problem 1
+    type Add = (numArray: number[]) => number
+    const sumArray: Add = (digits) => {
+        let sum = 0
+        for (let index = 0; index < digits.length; index++) {
+            sum = sum + digits[index];
+        }
+        return sum
+    }
+    const result=sumArray([1, 2, 3, 4, 5])
+    console.log(result)
 }
